c033fcb461a7fd8b7564f9548d75a59134a12f74
jedioncrk <dpeng1@gmail.com>
2019-10-08 11:35:18 -0700

    Add supervisor

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,12 @@ FROM yobasystems/alpine:3.9.0-amd64
 RUN apk update
 
 # install git
-RUN apk add coreutils git nginx openssh openrc
+RUN apk add --no-cache coreutils git nginx openssh openrc supervisor
 
 # add scripts
 ADD scripts/start.sh /start.sh
 ADD scripts/pull /usr/bin/pull
 ADD scripts/push /usr/bin/push
-ADD scripts/letsencrypt-setup /usr/bin/letsencrypt-setup
-ADD scripts/letsencrypt-renew /usr/bin/letsencrypt-renew
 
 # openssh conf
 RUN echo "Port 2222" >> /etc/ssh/sshd_config && \
@@ -20,22 +18,25 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && \
 echo "root:Docker!" | /usr/sbin/chpasswd
 
 # setup permissions
-RUN chmod 755 /usr/bin/pull && chmod 755 /usr/bin/push && chmod 755 /usr/bin/letsencrypt-setup && chmod 755 /usr/bin/letsencrypt-renew && chmod 755 /start.sh
+RUN chmod 755 /usr/bin/pull && chmod 755 /usr/bin/push && chmod 755 /start.sh
 
 # copy our nginx config
 RUN rm -Rf /etc/nginx/nginx.conf
 ADD conf/nginx.conf /etc/nginx/nginx.conf
+ADD conf/supervisord.conf /etc/supervisord.conf
 
 # setup nginx site conf
-RUN rm -Rf /etc/nginx/sites-available/*
-RUN mkdir -p /etc/nginx/sites-available/ && \
+RUN rm -Rf /etc/nginx/sites-available/* && mkdir -p /run/nginx && chown nginx:nginx /run/nginx && \
+mkdir -p /etc/nginx/sites-available/ && \
 mkdir -p /etc/nginx/sites-enabled/ && \
-mkdir -p /etc/nginx/ssl/ && \
 rm -Rf /var/www/* && \
 mkdir /var/www/html/
 ADD conf/nginx-site.conf /etc/nginx/sites-available/default.conf
 ADD conf/nginx-site-ssl.conf /etc/nginx/sites-available/default-ssl.conf
-RUN ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+RUN ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf && \
+# forward request and error logs to docker log collector
+ln -sf /dev/stdout /var/log/nginx/access.log && \
+ln -sf /dev/stderr /var/log/nginx/error.log
 
 # run post commands
 EXPOSE 443 80
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,9 +6,10 @@ mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
 if [ ! -z "$SSH_KEY" ]; then
- echo $SSH_KEY > /root/.ssh/id_rsa.base64
- base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
- chmod 600 /root/.ssh/id_rsa
+  echo $SSH_KEY > /root/.ssh/id_rsa.base64
+  base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+  rm /root/.ssh/id_rsa.base64
 fi
 
 # Set custom webroot
@@ -21,23 +22,43 @@ fi
 
 # Setup git variables
 if [ ! -z "$GIT_EMAIL" ]; then
- git config --global user.email "$GIT_EMAIL"
+  git config --global user.email "$GIT_EMAIL"
 fi
 if [ ! -z "$GIT_NAME" ]; then
- git config --global user.name "$GIT_NAME"
- git config --global push.default simple
+  git config --global user.name "$GIT_NAME"
+  git config --global push.default simple
 fi
 
 # Dont pull code down if the .git folder exists
 if [ ! -d "/var/www/html/.git" ]; then
  # Pull down code from git for our site!
  if [ ! -z "$GIT_REPO" ]; then
-   # Remove the test index file
-   rm -Rf /var/www/html/index.php
-   if [ ! -z "$GIT_BRANCH" ]; then
-     git clone -b $GIT_BRANCH $GIT_REPO /var/www/html
+   # Remove the test index file if you are pulling in a git repo
+   if [ ! -z ${REMOVE_FILES} ] && [ ${REMOVE_FILES} == 0 ]; then
+     echo "skiping removal of files"
    else
-     git clone $GIT_REPO /var/www/html
+     rm -Rf /var/www/html/*
+   fi
+   GIT_COMMAND='git clone '
+   if [ ! -z "$GIT_BRANCH" ]; then
+     GIT_COMMAND=${GIT_COMMAND}" -b ${GIT_BRANCH}"
+   fi
+
+   if [ -z "$GIT_USERNAME" ] && [ -z "$GIT_PERSONAL_TOKEN" ]; then
+     GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+   else
+    if [[ "$GIT_USE_SSH" == "1" ]]; then
+      GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+    else
+      GIT_COMMAND=${GIT_COMMAND}" https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_REPO}"
+    fi
+   fi
+   ${GIT_COMMAND} /var/www/html || exit 1
+   if [ -z "$SKIP_CHOWN" ]; then
+     chown -Rf nginx.nginx /var/www/html
    fi
  fi
 fi
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
